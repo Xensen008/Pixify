bd26f82c809ccd26192ef059d36e7f69fff33c4d
Xensen008 <arnabjyotikakati008@gmail.com>
2024-10-24 23:18:52 +0530

    bottombar is done

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,8 +1,27 @@
 
+import { bottombarLinks } from "@/constants";
+import { useLocation, Link } from "react-router-dom"
 
 const Bottombar = () => {
+  const {pathname }= useLocation();
   return (
-    <div>Bottombar</div>
+
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+            const isActive = pathname === link.route;
+            return (
+                <Link
+                  to={link.route}
+                  key={link.label}
+                  className={`${isActive && 'bg-primary-500 rounded-lg'} flex-center flex-col gap-1 pb-1 pt-2 px-2`}
+                >
+                  <img src={link.imgURL} width={16} height={16} alt={link.label} className={`${isActive && 'invert-white'}`} />
+                  <p className="text-tiny text-light-2" >{link.label}</p>
+                </Link>
+
+            )
+          })}
+    </section>
   )
 }
 
