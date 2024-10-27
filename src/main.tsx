bf7767b630fb90e22d42457da97b11d69650fad0
Xensen008 <arnabjyotikakati008@gmail.com>
2024-10-27 23:07:52 +0530

    added vercel file and improve authimg and added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
 import QueryProvider from "./lib/react-query/QueryProvider";
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <QueryProvider>
        <AuthProvider>
           <App />
+          <Analytics />
        </AuthProvider>
       </QueryProvider>
     </BrowserRouter>
